4ebc5555bdb5b2b28563251ae13e9e62547904c7
Samuel Huang <samhuang91@gmail.com>
2020-08-02 22:49:29 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Sam Huang on 8/2/20.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
