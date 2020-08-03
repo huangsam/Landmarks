03fd51b46ea23412391e4eff2a95725d6acc112d
Samuel Huang <samhuang91@gmail.com>
2020-08-02 23:17:16 -0700

    Implement circle image

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sam Huang on 8/2/20.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
