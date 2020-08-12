517531e6c3e22f7f749ef850aed1d136e97ac64d
Samuel Huang <samhuang91@gmail.com>
2020-08-12 07:10:14 -0700

    Implement LandmarkList view

---
+++ Landmarks/Landmark Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Sam Huang on 8/12/20.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarkData, id: \.id) {
+            landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
